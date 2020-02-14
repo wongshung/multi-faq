f582f48b25bab127abc6c3c489b2b248f59fc3cf
lipinghuang <lipinghuang@emotibot.com>
2020-02-14 16:43:42 +0800

    SZTECH-1681 - training set could be configured.

--- src/main/java/com/hackathon/ceptional/config/DataInitializer.java
+++ src/main/java/com/hackathon/ceptional/config/DataInitializer.java
@@ -3,6 +3,7 @@ package com.hackathon.ceptional.config;
 import com.hackathon.ceptional.service.FaqDataService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import java.io.File;
@@ -18,6 +19,12 @@ import java.io.File;
 @Slf4j
 public class DataInitializer implements CommandLineRunner {
 
+    /**
+     * training set
+     */
+    @Value("${faq.training.set}")
+    private String trainingSet = "Hackathon_P1_trainingSet.xlsx";
+
     private FaqDataService dataInitService;
     @Autowired
     private void setDataInitService(FaqDataService service) {
@@ -27,8 +34,8 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         log.info("DataInitializer start to initialize ...");
-        String configPath = System.getProperty("user.dir")+File.separator+"config";
-        String filePath = configPath.concat("/Hackathon_P1_trainingSet.xlsx");
+        String configPath = System.getProperty("user.dir")+File.separator+"config/";
+        String filePath = configPath.concat(trainingSet);
         try {
             File excelFile = new File(filePath);
             dataInitService.initData(excelFile);
